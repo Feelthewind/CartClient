e0f04ecc4ff207c3b5fac84ac680e9bbf7ffec89
lcs <skyisthelimitlessW@gmail.com>
2019-03-19 19:52:16 +0900

    select product varations

--- components/products/ProductVariation.vue
+++ components/products/ProductVariation.vue
@@ -6,15 +6,20 @@
     <div class="control">
       <div class="select is-fullwidth">
         <select
-          name=""
-          id=""
+          :value="selectedVariationId"
+          @change="changed($event, type)"
         >
           <option value="">Please choose</option>
           <option
             v-for="variation in variations"
             :key="variation.id"
+            :value="variation.id"
           >
             {{ variation.name }}
+
+            <template v-if="variation.price_varies">
+              ({{ variation.price }})
+            </template>
           </option>
         </select>
       </div>
@@ -32,6 +37,36 @@ export default {
     variations: {
       required: true,
       type: Array
+    },
+    value: {
+      required: false,
+      default: ""
+    }
+  },
+
+  computed: {
+    selectedVariationId() {
+      if (!this.findVariation(this.value.id)) {
+        return "";
+      }
+
+      return this.value.id;
+    }
+  },
+
+  methods: {
+    changed(event, type) {
+      this.$emit("input", this.findVariation(event.target.value));
+    },
+
+    findVariation(id) {
+      let variation = this.variations.find(v => v.id == id);
+
+      if (typeof variation === "undefined") {
+        return null;
+      }
+
+      return variation;
     }
   }
 };
--- pages/products/_slug.vue
+++ pages/products/_slug.vue
@@ -31,8 +31,32 @@
                 :type="type"
                 :variations="variations"
                 :key="type"
+                v-model="form.variation"
               />
+
+              <div
+                class="field has-addons"
+                v-if="form.variation"
+              >
+                <div class="control">
+                  <div class="select is-fullwidth">
+                    <select
+                      name=""
+                      id=""
+                    >
+                      <option value="">1</option>
+                    </select>
+                  </div>
+                </div>
+                <div class="control">
+                  <button
+                    type="submit"
+                    class="button is-info"
+                  >Add to cart</button>
+                </div>
+              </div>
             </form>
+
           </section>
         </div>
       </div>
@@ -45,7 +69,11 @@ import ProductVariation from "@/components/products/ProductVariation";
 export default {
   data() {
     return {
-      product: null
+      product: null,
+      form: {
+        variation: "",
+        quantity: 1
+      }
     };
   },
 
