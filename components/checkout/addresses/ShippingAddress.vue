59fb3d8f657b1b07e373d5b6a517f69bd2905273
lcs <skyisthelimitlessW@gmail.com>
2019-03-27 00:56:17 +0900

    switching the shipping address

--- components/checkout/addresses/ShippingAddress.vue
+++ components/checkout/addresses/ShippingAddress.vue
@@ -3,24 +3,55 @@
     <div class="message-body">
       <h1 class="title is-5">Ship to</h1>
 
-      <template v-if="selectedAddress">
-        {{ selectedAddress.name }}<br>
-        {{ selectedAddress.address_1 }}<br>
-        {{ selectedAddress.city }}<br>
-        {{ selectedAddress.postal_code }}<br>
-        {{ selectedAddress.country.name }}<br>
+      <template v-if="selecting">
+        <ShippingAddressSelector
+          :addresses="addresses"
+          :selectedAddress="selectedAddress"
+          @click="addressSelected"
+        />
       </template>
+
+      <template v-else>
+        <template v-if="selectedAddress">
+          <p>
+            {{ selectedAddress.name }}<br>
+            {{ selectedAddress.address_1 }}<br>
+            {{ selectedAddress.city }}<br>
+            {{ selectedAddress.postal_code }}<br>
+            {{ selectedAddress.country.name }}<br>
+          </p>
+
+          <br>
+        </template>
+        <div class="field is-grouped">
+          <p class="control">
+            <a
+              href=""
+              class="button is-info"
+              @click.prevent="selecting = true"
+            >Change shipping address</a>
+          </p>
+        </div>
+      </template>
+
     </div>
   </article>
 </template>
 
 <script>
+import ShippingAddressSelector from "./ShippingAddressSelector";
+
 export default {
   data() {
     return {
+      selecting: false,
       localAddresses: this.addresses,
       selectedAddress: null
     };
+  },
+
+  components: {
+    ShippingAddressSelector
   },
 
   props: {
@@ -37,6 +68,10 @@ export default {
   },
 
   methods: {
+    addressSelected(address) {
+      this.switchAddress(address);
+      this.selecting = false;
+    },
     switchAddress(address) {
       this.selectedAddress = address;
     }
